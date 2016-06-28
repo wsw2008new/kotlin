fa7292714a2893105578f0574998a69964274cef
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-06-28 20:57:35 +0300

    Move exceptions to kotlin package for JS, introduce type aliases for exceptions in kotlin package for JVM

--- libraries/stdlib/src/kotlin/util/Exceptions.kt
+++ libraries/stdlib/src/kotlin/util/Exceptions.kt
@@ -4,8 +4,20 @@
 @file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN", "DeprecatedCallableAddReplaceWith")
 package kotlin
 
+public typealias Error = java.lang.Error
+public typealias Exception = java.lang.Exception
+public typealias RuntimeException = java.lang.RuntimeException
+public typealias IllegalArgumentException = java.lang.IllegalArgumentException
+public typealias IllegalStateException = java.lang.IllegalStateException
+public typealias IndexOutOfBoundsException = java.lang.IndexOutOfBoundsException
+public typealias UnsupportedOperationException = java.lang.UnsupportedOperationException
 
-typealias NoSuchElementException = java.util.NoSuchElementException
+public typealias NumberFormatException = java.lang.NumberFormatException
+public typealias NullPointerException = java.lang.NullPointerException
+public typealias ClassCastException = java.lang.ClassCastException
+public typealias AssertionError = java.lang.AssertionError
+
+public typealias NoSuchElementException = java.util.NoSuchElementException
 
 /**
  * Returns an array of stack trace elements representing the stack trace
