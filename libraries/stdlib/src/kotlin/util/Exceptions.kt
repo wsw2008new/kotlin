bbbbe8f4ebd2393af7e6c0fd8a6d472b6e6ca054
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-06-29 21:04:08 +0300

    Typealias in builtins, take #2

--- libraries/stdlib/src/kotlin/util/Exceptions.kt
+++ libraries/stdlib/src/kotlin/util/Exceptions.kt
@@ -4,18 +4,18 @@
 @file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN", "DeprecatedCallableAddReplaceWith")
 package kotlin
 
-public typealias Error = java.lang.Error
-public typealias Exception = java.lang.Exception
-public typealias RuntimeException = java.lang.RuntimeException
-public typealias IllegalArgumentException = java.lang.IllegalArgumentException
-public typealias IllegalStateException = java.lang.IllegalStateException
-public typealias IndexOutOfBoundsException = java.lang.IndexOutOfBoundsException
-public typealias UnsupportedOperationException = java.lang.UnsupportedOperationException
-
-public typealias NumberFormatException = java.lang.NumberFormatException
-public typealias NullPointerException = java.lang.NullPointerException
-public typealias ClassCastException = java.lang.ClassCastException
-public typealias AssertionError = java.lang.AssertionError
+//public typealias Error = java.lang.Error
+//public typealias Exception = java.lang.Exception
+//public typealias RuntimeException = java.lang.RuntimeException
+//public typealias IllegalArgumentException = java.lang.IllegalArgumentException
+//public typealias IllegalStateException = java.lang.IllegalStateException
+//public typealias IndexOutOfBoundsException = java.lang.IndexOutOfBoundsException
+//public typealias UnsupportedOperationException = java.lang.UnsupportedOperationException
+//
+//public typealias NumberFormatException = java.lang.NumberFormatException
+//public typealias NullPointerException = java.lang.NullPointerException
+//public typealias ClassCastException = java.lang.ClassCastException
+//public typealias AssertionError = java.lang.AssertionError
 
 public typealias NoSuchElementException = java.util.NoSuchElementException
 
@@ -26,3 +26,7 @@ public typealias NoSuchElementException = java.util.NoSuchElementException
 @Deprecated("Provided for binary compatibility", level = DeprecationLevel.HIDDEN)
 public val Throwable.stackTrace: Array<StackTraceElement>
     get() = stackTrace!!
+
+fun exception_test() {
+    throw NegativeArraySizeException()
+}
