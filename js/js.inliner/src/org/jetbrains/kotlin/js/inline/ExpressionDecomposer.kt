ed147c6800a8a3ac4ff2d6b03310f3a31441a9d9
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-07-01 12:14:05 +0300

    KT-12928: decompose invocation like "a.foo(b)" to "$t = a; $s = $t.foo.bind($t); $s(b)" instead of "$t = a.foo; $t(b)", since in the latter case `foo` won't receive proper `this`. Add optimization that replaces "foo.bar.bind(baz)(args)" with "baz.bar(args)"

--- js/js.inliner/src/org/jetbrains/kotlin/js/inline/ExpressionDecomposer.kt
+++ js/js.inliner/src/org/jetbrains/kotlin/js/inline/ExpressionDecomposer.kt
@@ -20,6 +20,7 @@ import com.google.dart.compiler.backend.js.ast.*
 import com.google.dart.compiler.backend.js.ast.metadata.*
 import com.intellij.util.SmartList
 import org.jetbrains.kotlin.js.inline.util.IdentitySet
+import org.jetbrains.kotlin.js.translate.utils.JsAstUtils
 import org.jetbrains.kotlin.js.translate.utils.JsAstUtils.*
 import org.jetbrains.kotlin.js.translate.utils.jsAstUtils.*
 
@@ -267,13 +268,17 @@ internal class ExpressionDecomposer private constructor(
             // Qualifier might be a reference to lambda property. See KT-7674
             // An exception here is `fn.call()`, which are marked as side effect free. Further recognition of such
             // case in inliner might be quite difficult, so never extract such call (and other calls marked this way).
-             if ((qualifier !in containsNodeWithSideEffect || (qualifier as? HasMetadata)?.sideEffects == SideEffectKind.PURE) &&
-                 (callee != null && receiver != null && receiver in containsNodeWithSideEffect)
-             ) {
+            if ((qualifier !in containsNodeWithSideEffect || (qualifier as? HasMetadata)?.sideEffects == SideEffectKind.PURE) &&
+                (callee != null && receiver != null && receiver in containsNodeWithSideEffect)
+            ) {
                 val receiverTmp = receiver.extractToTemporary()
                 callee.qualifier = receiverTmp
             }
             else {
+                if (receiver != null && callee != null) {
+                    val receiverTmp = receiver.extractToTemporary()
+                    qualifier = JsAstUtils.invokeBind(receiverTmp, pureFqn(callee.ident, receiverTmp))
+                }
                 qualifier = qualifier.extractToTemporary()
             }
         }
