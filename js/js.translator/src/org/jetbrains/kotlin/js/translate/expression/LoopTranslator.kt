32e2b61ca6397e22c7b845dfea48c4a6ad197eab
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-07-15 16:33:06 +0300

    ~ fixup e90969d7ae6f6ae411b9ed81d9896e7fccea3407

--- js/js.translator/src/org/jetbrains/kotlin/js/translate/expression/LoopTranslator.kt
+++ js/js.translator/src/org/jetbrains/kotlin/js/translate/expression/LoopTranslator.kt
@@ -101,16 +101,14 @@ fun translateForExpression(expression: KtForExpression, context: TranslationCont
 
     val destructuringParameter: KtDestructuringDeclaration? = expression.destructuringParameter
 
-    fun declareParameter(): Pair<JsName, Boolean> {
-        val loopParameter = getLoopParameter(expression)
-        if (loopParameter != null) {
-            return Pair(context.getNameForElement(loopParameter), false)
-        }
-        assert(destructuringParameter != null) { "If loopParameter is null, multi parameter must be not null ${expression.text}" }
-        return Pair(context.scope().declareTemporary(), true)
+    val loopParameter = getLoopParameter(expression)
+    val (parameterName, parameterIsTemporary) = if (loopParameter != null) {
+        Pair(context.getNameForElement(loopParameter), false)
     }
-
-    val (parameterName, parameterIsTemporary) = declareParameter()
+    else {
+        assert(destructuringParameter != null) { "If loopParameter is null, multi parameter must be not null ${expression.text}" }
+        Pair(context.scope().declareTemporary(), true)
+    }
 
     fun translateBody(itemValue: JsExpression?): JsStatement? {
         val realBody = expression.body?.let { Translation.translateAsStatementAndMergeInBlockIfNeeded(it, context) }
